34481e3df07283262c46daa728e6711999cf07c2
Rajesh <mekalarajesh812@gmail.com>
2026-02-18 08:56:49 +0530

    Cleanup for vercel

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -7,9 +8,8 @@ app = FastAPI()
 # Serve static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Templates folder
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def home(request: Request):
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
